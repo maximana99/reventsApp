2ad8dee83c3237f60d0b272e67c9c3f0c2a011de
Ana Maria Maxim <maximana1999@yahoo.com>
2023-06-05 17:52:41 +0300

    selecting an event to read

--- revents/src/features/events/eventDashboard/EventDashboard.jsx
+++ revents/src/features/events/eventDashboard/EventDashboard.jsx
@@ -4,7 +4,12 @@ import EventList from "./EventList";
 import EventForm from "../eventForm/EventForm";
 import { sampleData } from "../../../app/api/sampleData";
 
-export default function EventDashboard({ formOpen, setFormOpen }) {
+export default function EventDashboard({
+  formOpen,
+  setFormOpen,
+  selectEvent,
+  selectedEvent,
+}) {
   const [events, setEvents] = useState(sampleData);
 
   function handleCreateEvent(event) {
@@ -14,7 +19,7 @@ export default function EventDashboard({ formOpen, setFormOpen }) {
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={events} />
+        <EventList events={events} selectEvent={selectEvent} />
       </Grid.Column>
       <Grid.Column width={6}>
         {formOpen && (
@@ -22,6 +27,7 @@ export default function EventDashboard({ formOpen, setFormOpen }) {
             setFormOpen={setFormOpen}
             setEvents={setEvents}
             createEvent={handleCreateEvent}
+            selectedEvent={selectedEvent}
           />
         )}
       </Grid.Column>
