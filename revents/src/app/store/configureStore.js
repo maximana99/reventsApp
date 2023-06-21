38543d3feaa12ed35662c1a4f96483eb039dc307
Ana Maria Maxim <maximana1999@yahoo.com>
2023-06-21 21:58:01 +0300

    redux thunk and isolating loading buttons

--- revents/src/app/store/configureStore.js
+++ revents/src/app/store/configureStore.js
@@ -1,7 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import rootReducer from "./rootReducer";
+import thunk from "redux-thunk";
 
 export function configureStore() {
-  return createStore(rootReducer, composeWithDevTools());
+  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 }
