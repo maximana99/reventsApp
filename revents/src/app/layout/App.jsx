1661cfd58bb9aaed86e0b46abd1d4c6b6701b3d2
Ana Maria Maxim <maximana1999@yahoo.com>
2023-07-01 01:50:18 +0300

    added a profile page

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -13,6 +13,7 @@ import ErrorComponent from "../common/errors/ErrorComponent";
 import AccountPage from "../../features/auth/AccountPage";
 import { useSelector } from "react-redux";
 import LoadingComponent from "./LoadingComponent";
+import ProfilePage from "../../features/profiles/profilePage/ProfilePage";
 
 function App() {
   const { key } = useLocation();
@@ -32,6 +33,7 @@ function App() {
         <Route path={"/createEvent"} element={<EventForm key={key} />} />
         <Route path={"/manage/:id"} element={<EventForm key={key} />} />
         <Route path='/account' element={<AccountPage />} />
+        <Route path='/profile/:id' element={<ProfilePage />} />
         <Route path='/error' element={<ErrorComponent />} />
       </Routes>
     </Container>
